2bd7b3128f3254f8c1ec814a5450051eeefc6bf2
LeoD3v <leoargusa1991@gmail.com>
2023-09-07 01:20:50 +0200

    i think im finished

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,210 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios
+      .get(`http://localhost:8000/characters`)
+      .then((response) => {
+        console.log("Data:", response.data);
 
+        const characterListElement = document.querySelector(
+          ".characters-container"
+        );
+        const characters = response.data;
+
+        const characterListHTML = characters
+          .map((character) => {
+            return `
+            <div class="character-info">
+              <div class="name">${character.name}</div>
+              <div class="occupation">${character.occupation}</div>
+              <div class="cartoon">${character.cartoon ? "Yes" : "No"}</div>
+              <div class="weapon">${character.weapon}</div>
+            </div>
+          `;
+          })
+          .join("");
+
+        characterListElement.innerHTML = characterListHTML;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
-  getOneRegister () {
+  getOneRegister(id) {
+    axios
+      .get(`http://localhost:8000/characters/${id}`)
+      .then((response) => {
+        const characterData = response.data;
+        const characterListElement = document.querySelector(
+          ".characters-container"
+        );
 
+        const characterInfoDiv = document.createElement("div");
+        characterInfoDiv.classList.add("character-info");
+
+        const nameDiv = document.createElement("div");
+        nameDiv.classList.add("name");
+        nameDiv.textContent = `Name: ${characterData.name}`;
+
+        const occupationDiv = document.createElement("div");
+        occupationDiv.classList.add("occupation");
+        occupationDiv.textContent = `Occupation: ${characterData.occupation}`;
+
+        const cartoonDiv = document.createElement("div");
+        cartoonDiv.classList.add("cartoon");
+        cartoonDiv.textContent = `Cartoon: ${
+          characterData.cartoon ? "yes" : "no"
+        }`;
+
+        const weaponDiv = document.createElement("div");
+        weaponDiv.classList.add("weapon");
+        weaponDiv.textContent = `Weapon: ${characterData.weapon}`;
+
+        characterInfoDiv.appendChild(nameDiv);
+        characterInfoDiv.appendChild(occupationDiv);
+        characterInfoDiv.appendChild(weaponDiv);
+        characterInfoDiv.appendChild(cartoonDiv);
+
+        characterListElement.appendChild(characterInfoDiv);
+      })
+      .catch((error) => {
+        error;
+      });
   }
 
-  createOneRegister () {
+  createOneRegister(createNew) {
+    axios
+      .post(`http://localhost:8000/characters`, createNew)
+      .then((response) => {
+        console.log(response);
+        const characterData = response.data;
+        const characterListElement = document.querySelector(
+          ".characters-container"
+        );
 
+        const characterInfoDiv = document.createElement("div");
+        characterInfoDiv.classList.add("character-info");
+
+        const nameDiv = document.createElement("div");
+        nameDiv.classList.add("name");
+        nameDiv.textContent = `Name: ${characterData.name}`;
+
+        const occupationDiv = document.createElement("div");
+        occupationDiv.classList.add("occupation");
+        occupationDiv.textContent = `Occupation: ${characterData.occupation}`;
+
+        const cartoonDiv = document.createElement("div");
+        cartoonDiv.classList.add("cartoon");
+        cartoonDiv.textContent = `Cartoon: ${
+          characterData.cartoon ? "yes" : "no"
+        }`;
+
+        const weaponDiv = document.createElement("div");
+        weaponDiv.classList.add("weapon");
+        weaponDiv.textContent = `Weapon: ${characterData.weapon}`;
+
+        characterInfoDiv.appendChild(nameDiv);
+        characterInfoDiv.appendChild(occupationDiv);
+        characterInfoDiv.appendChild(weaponDiv);
+        characterInfoDiv.appendChild(cartoonDiv);
+
+        characterListElement.appendChild(characterInfoDiv);
+        console.log(response);
+      })
+      .catch((err) => {
+        err;
+      });
   }
 
-  updateOneRegister () {
+  updateOneRegister(edit, id) {
+    axios
+      .put(`http://localhost:8000/characters/${id}`, edit)
+      .then((response) => {
+        console.log(response);
+        const characterData = response.data;
+        const characterListElement = document.querySelector(
+          ".characters-container"
+        );
 
+        const characterInfoDiv = document.createElement("div");
+        characterInfoDiv.classList.add("character-info");
+
+        const nameDiv = document.createElement("div");
+        nameDiv.classList.add("name");
+        nameDiv.textContent = `Name: ${characterData.name}`;
+
+        const occupationDiv = document.createElement("div");
+        occupationDiv.classList.add("occupation");
+        occupationDiv.textContent = `Occupation: ${characterData.occupation}`;
+
+        const cartoonDiv = document.createElement("div");
+        cartoonDiv.classList.add("cartoon");
+        cartoonDiv.textContent = `Cartoon: ${
+          characterData.cartoon ? "yes" : "no"
+        }`;
+
+        const weaponDiv = document.createElement("div");
+        weaponDiv.classList.add("weapon");
+        weaponDiv.textContent = `Weapon: ${characterData.weapon}`;
+
+        characterInfoDiv.appendChild(nameDiv);
+        characterInfoDiv.appendChild(occupationDiv);
+        characterInfoDiv.appendChild(weaponDiv);
+        characterInfoDiv.appendChild(cartoonDiv);
+
+        characterListElement.appendChild(characterInfoDiv);
+      })
+      .catch((err) => {
+        err;
+      });
   }
 
-  deleteOneRegister () {
+  deleteOneRegister(id) {
+    axios
+      .delete(`http://localhost:8000/characters/${id}`)
+      .then((response) => {
+        console.log("id:", id, response.data);
 
+        const characterData = response.data;
+        const characterListElement = document.querySelector(
+          ".characters-container"
+        );
+
+        const characterInfoDiv = document.createElement("div");
+        characterInfoDiv.classList.add("character-info");
+
+        const nameDiv = document.createElement("div");
+        nameDiv.classList.add("name");
+        nameDiv.textContent = `Name: ${characterData.name}`;
+
+        const occupationDiv = document.createElement("div");
+        occupationDiv.classList.add("occupation");
+        occupationDiv.textContent = `Occupation: ${characterData.occupation}`;
+
+        const cartoonDiv = document.createElement("div");
+        cartoonDiv.classList.add("cartoon");
+        cartoonDiv.textContent = `Cartoon: ${
+          characterData.cartoon ? "yes" : "no"
+        }`;
+
+        const weaponDiv = document.createElement("div");
+        weaponDiv.classList.add("weapon");
+        weaponDiv.textContent = `Weapon: ${characterData.weapon}`;
+
+        characterInfoDiv.appendChild(nameDiv);
+        characterInfoDiv.appendChild(occupationDiv);
+        characterInfoDiv.appendChild(weaponDiv);
+        characterInfoDiv.appendChild(cartoonDiv);
+
+        characterListElement.appendChild(characterInfoDiv);
+        res;
+      })
+      .catch((error) => {
+        error;
+      });
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,82 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      const characterListElement = document.querySelector(
+        ".characters-container"
+      );
+      characterListElement.innerHTML = "";
+      charactersAPI.getFullList();
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      const characterListElement = document.querySelector(
+        ".characters-container"
+      );
+      characterListElement.innerHTML = "";
+      const characterId = document.querySelector(
+        'input[name="character-id"]'
+      ).value;
+      charactersAPI.getOneRegister(characterId);
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {
+      const characterListElement = document.querySelector(
+        ".characters-container"
+      );
+      characterListElement.innerHTML = "";
+      const characterId = document.querySelector(
+        'input[name="character-id-delete"]'
+      ).value;
 
-  });
+      charactersAPI.deleteOneRegister(characterId);
+    });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const characterListElement = document.querySelector(
+        ".characters-container"
+      );
+      characterListElement.innerHTML = "";
+      const characterId = document.querySelector('input[name="chr-id"]').value;
+      const editOne = {
+        id: document.getElementById("edit-character-form").elements[0].value,
+        name: document.getElementById("edit-character-form").elements[1].value,
+        occupation: document.getElementById("edit-character-form").elements[2]
+          .value,
+        weapon: document.getElementById("edit-character-form").elements[3]
+          .value,
+        cartoon: document.getElementById("edit-character-form").elements[4]
+          .value,
+      };
+      console.log(editOne);
+      charactersAPI.updateOneRegister(editOne, characterId);
+    });
 
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const characterListElement = document.querySelector(
+        ".characters-container"
+      );
+      characterListElement.innerHTML = "";
+      const createNew = {
+        name: document.getElementById("new-character-form").elements[0].value,
+        occupation:
+          document.getElementById("new-character-form").elements[1].value,
+        weapon: document.getElementById("new-character-form").elements[2].value,
+        cartoon:
+          document.getElementById("new-character-form").elements[3].value,
+      };
+      charactersAPI.createOneRegister(createNew);
+    });
 });
